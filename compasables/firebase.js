8743e558e0084469e0a664ba334fa2896f4c4812
Roland Clarion <131647107+yoyan1@users.noreply.github.com>
2024-07-05 06:16:20 -0600

    Update firebase.js

--- compasables/firebase.js
+++ compasables/firebase.js
@@ -6,13 +6,7 @@ import { getStorage } from 'firebase/storage'
 
 export const firebaseApp = initializeApp({
   // your application settings
-  apiKey: "AIzaSyANm3upeIbvRZ2sEOfUQjt9iSTGgR2jnHg",
-  authDomain: "rmes-monitoring-system.firebaseapp.com",
-  projectId: "rmes-monitoring-system",
-  storageBucket: "rmes-monitoring-system.appspot.com",
-  messagingSenderId: "610369031733",
-  appId: "1:610369031733:web:57a4a5ece2c9fa3c982b7c",
-  measurementId: "G-BRZNQNTLP1"
+  
   
 })
 
