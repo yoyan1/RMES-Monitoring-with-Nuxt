052ece596755cc71e19479701c689c44d6e8db39
yoyan1 <yoyan947@gmail.com>
2024-07-07 10:27:20 -0600

    redesign

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,13 +4,7 @@ export default defineNuxtConfig({
   modules: ['@nuxt/ui', "@nuxt/icon", "nuxt-vuefire"],
   vuefire: {
     config: {
-      apiKey: "AIzaSyANm3upeIbvRZ2sEOfUQjt9iSTGgR2jnHg",
-      authDomain: "rmes-monitoring-system.firebaseapp.com",
-      projectId: "rmes-monitoring-system",
-      storageBucket: "rmes-monitoring-system.appspot.com",
-      messagingSenderId: "610369031733",
-      appId: "1:610369031733:web:57a4a5ece2c9fa3c982b7c",
-      measurementId: "G-BRZNQNTLP1"
+     
       // there could be other properties depending on the project
     },
   },
